bf7421764b36181c059b7ecd87ab15c755831b7a
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-06-30 12:14:03 +0300

    Add Cryptocurrencie card and fetch cryptocurrencies

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,18 +1,4 @@
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       limit: '50',
-//       offset: '0',
-//       orderBy: '24hVolume',
-//       orderDirection: 'desc'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': 'e9b310d4dbmsh038d285551e639ep1ac433jsn3e83b0e98725',
-//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//     }
-//   };
+
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
