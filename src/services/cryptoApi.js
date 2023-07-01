9e535e87058959d73a725b37813579f7470a9d9c
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-07-01 15:40:19 +0300

    Add Search

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,8 +3,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Key': 'e9b310d4dbmsh038d285551e639ep1ac433jsn3e83b0e98725',
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
